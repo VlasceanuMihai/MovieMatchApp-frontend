b21b8c066e371663373946a14b0ae46131804d0d
VlasceanuMihai <mihai.vlasceanu1996@gmail.com>
2021-02-19 18:59:28 +0200

    Fix /registration request & clean input values from fields

--- src/APIs/Endpoints.js
+++ src/APIs/Endpoints.js
@@ -3,5 +3,14 @@ import axios from "axios";
 
 export const BASE_API = "http://localhost:8080";
 
+const signUpHeaders = {
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
+};
+
 // Sign-up API
-export const signUp = () => `${BASE_API}/api/v1/registration`;
+export const signUpApi = (data) =>
+  axios.post(`${BASE_API}/api/v1/registration`, data, signUpHeaders);
