c432f955607afade41829e0cab01081699e9c495
VlasceanuMihai <mihai.vlasceanu1996@gmail.com>
2021-02-20 16:56:18 +0200

    Add design for login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import { Route } from "react-router-dom";
 import SignUpComponent from "./components/auth/SignUpComponent";
+import LoginComponent from "./components/auth/LoginComponent";
 
 function App() {
   return (
     <div className="App">
-      <SignUpComponent />
+      <Route path="/signup" exact component={SignUpComponent} />
+      <Route path="/register" exact component={SignUpComponent} />
+      <Route path="/login" exact component={LoginComponent} />
+      <Route path="/" exact component={LoginComponent} />
     </div>
   );
 }
