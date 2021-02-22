e6c03a2bcde4d7c18809c12593025bbffef7dea2
VlasceanuMihai <mihai.vlasceanu1996@gmail.com>
2021-02-22 18:51:09 +0200

    Fixed registration flow and added login design

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import SignUpComponent from "./components/auth/SignUpComponent";
 import LoginComponent from "./components/auth/LoginComponent";
+import Movies from "./components/menu/Movies";
+import Dashboard from "./components/menu/Dashboard";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Route path="/register" exact component={SignUpComponent} />
       <Route path="/login" exact component={LoginComponent} />
       <Route path="/" exact component={LoginComponent} />
+      <Route path="/home" exact component={Dashboard} />
+      <Route path="/movies" exact component={Movies} />
     </div>
   );
 }
