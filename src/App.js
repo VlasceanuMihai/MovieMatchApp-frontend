ccb2262ab7984028bbfca6eeaad638b1d82dcd93
VlasceanuMihai <mihai.vlasceanu1996@gmail.com>
2021-03-11 13:38:34 +0200

    Delete useless imports

--- src/App.js
+++ src/App.js
@@ -4,15 +4,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignUpComponent from "./components/auth/SignUpComponent";
 import LoginComponent from "./components/auth/LoginComponent";
 import ErrorComponent from "./components/error-handler/ErrorComponent";
-import LogoutComponent from "./components/auth/LogoutComponent";
 import AuthenticatedRoute from "./components/auth/AuthenticatedRoute";
 import DashboardLayout from "./layouts/dasboardLayout/DashboardLayout";
 import SettingsComponent from "./components/menu/settings/SettingsComponent";
-import Toolbar from "./components/menu/movies/Toolbar";
 import MoviesComponent from "./components/menu/movies/MoviesComponent";
 import "react-perfect-scrollbar/dist/css/styles.css";
-import NavBar from "./layouts/dasboardLayout/navbar/NavBar";
 import TopBarDashboard from "./layouts/dasboardLayout/TopBarDashboard";
+
 function App() {
   return (
     <div className="App">
